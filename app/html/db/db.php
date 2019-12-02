587ad5e72c527d2e55a86fba7393d06527aceb2c
Akatsuki507 <Alfonso.Alvarado.v31@gmail.com>
2019-12-02 06:18:45 -0500

    fk docentes_id y administrativos_id can be null

--- app/html/db/db.php
+++ app/html/db/db.php
@@ -1,10 +1,9 @@
 <?php
 class Conectar{
 	public static function conexion(){
-		$conexion= new mysqli("localhost:3306", "root", "12345", "db_administracion");
+		$conexion= new mysqli("ing-web-db", "root", "12345", "db_administracion");
 		if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;}
-    echo "ya valio madres";
 		$conexion->query("SET NAMES 'utf8'");
 		return $conexion;
 	}
