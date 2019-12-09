f7441b6028045cc5280f76d0023924bf8f9846f7
Akatsuki507 <Alfonso.Alvarado.v31@gmail.com>
2019-12-09 18:58:46 -0500

    docente y administrativo extends Model Base

--- app/app/models/administrativo.php
+++ app/app/models/administrativo.php
@@ -1,26 +1,10 @@
 <?php
 class Administrativo{
-    private $db;
-    private $docentes;
-    public $table = "docentes";
+    protected $table = "docentes";
  
     public function __construct(){
         $this->db=Conectar::conexion();
         $this->usuarios=array();
-    }
-    public function all(){
-        $consulta=$this->db->query("select * from {$this->table};");
-        while($filas=$consulta->fetch_assoc()){
-            $this->usuarios[]=$filas;
-        }
-        return $this->usuarios;
-    }
-    public function where($col, $data){
-        $consulta=$this->db->query("select * from {$this->table} where $col = $data;");
-        while($filas=$consulta->fetch_assoc()){
-            $this->usuarios[]=$filas;
-        }
-        return $this->usuarios;
     }
 }
 ?>
