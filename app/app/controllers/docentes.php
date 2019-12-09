668b392fd144ea029dc3b2d2d806fe00ea8ca474
Akatsuki507 <Alfonso.Alvarado.v31@gmail.com>
2019-12-09 18:36:39 -0500

    docentes amarrando views

--- app/app/controllers/docentes.php
+++ app/app/controllers/docentes.php
@@ -2,10 +2,12 @@
 class docentes extends controller{
 	public function index(){
 		echo 'docentes/index';
+		$this->view('docentes/index', []);
 	}
 
 	public function show(){
 		echo 'docentes/show';
+		$this->view('docentes/show', []);
 	}
 }
 ?>
