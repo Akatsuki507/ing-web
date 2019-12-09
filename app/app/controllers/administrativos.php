f11d65bc8b55d3fcadcf4dfbf8a9069020e8f97b
Akatsuki507 <Alfonso.Alvarado.v31@gmail.com>
2019-12-09 18:29:44 -0500

    administrativos model, views and controller

--- app/app/controllers/administrativos.php
+++ app/app/controllers/administrativos.php
@@ -2,10 +2,12 @@
 class administrativos extends controller{
 	public function index(){
 		echo 'administrativos/index';
+		$this->view('administrativos/index', []);
 	}
 
 	public function show(){
 		echo 'administrativos/show';
+		$this->view('administrativos/show', []);
 	}
 }
 ?>
