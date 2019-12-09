96b126b602ffcba4081a7ab67eb185730924ef37
Akatsuki507 <Alfonso.Alvarado.v31@gmail.com>
2019-12-09 18:20:06 -0500

    funcion default en app

--- app/app/core/app.php
+++ app/app/core/app.php
@@ -7,6 +7,16 @@ class App{
 	public function __construct(){
 		$url = $this->parseUrl();
 
+		$this->default($url);
+	}
+
+	public function parseUrl(){
+		if(isset($_GET['url'])) {
+			return $url = explode('/', filter_var(rtrim($_GET['url'],'/'), FILTER_SANITIZE_URL));
+		}
+	}
+
+	protected function default($url){
 		if (file_exists('../app/controllers/' . $url[0]. '.php')) {
 			$this->controller = $url[0];
 			unset($url[0]);
@@ -26,12 +36,6 @@ class App{
 		$this->params = $url ? array_values($url) : [];
 
 		call_user_func_array([$this->controller, $this->method], $this->params);
-	}
-
-	public function parseUrl(){
-		if(isset($_GET['url'])) {
-			return $url = explode('/', filter_var(rtrim($_GET['url'],'/'), FILTER_SANITIZE_URL));
-		}
 	}
 }
 ?>
