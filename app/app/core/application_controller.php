b1c53dda7ae07e8051cfd376a6821866a6b7b475
Akatsuki507 <Alfonso.Alvarado.v31@gmail.com>
2019-12-09 15:14:50 -0500

    .ht.htaccel htaccess is working

--- app/app/core/application_controller.php
+++ app/app/core/application_controller.php
@@ -5,12 +5,12 @@ class application{
 	protected $method = 'index';
 	protected $params = [];
 	public function __construct(){
-		$this->parseUrl();
+		$url = $this->parseUrl();
 	}
 
 	public function parseUrl(){
 		if(isset($_GET['url'])) {
-			echo $_GET['url'];
+			return $url = explode('/', filter_var(rtrim($_GET['url'],'/'), FILTER_SANITIZE_URL));
 		}
 	}
 }
