4bdb5b7541ff4e3d96aa72a8bb14662e74d16b6a
Akatsuki507 <Alfonso.Alvarado.v31@gmail.com>
2019-12-15 14:22:24 -0500

    documentando controlador base

--- app/app/core/controller.php
+++ app/app/core/controller.php
@@ -1,10 +1,12 @@
 <?php
 class controller{
 
+	//metodo que permite amarrar las vistas a los controladores y el $data son los parametros que les pasaremos a las vistas para poderlos manipular en dicha vista
 	public function view($view, $data = []){
 		require_once '../app/views/' . $view . '.phtml';
 	}
 
+	//metodo que verifica si el usuario tiene una sesion activa, este metodo se llama desde casi todos los demas metodos
 	public function autenticate(){
 		$contenido = $_COOKIE["id_user_sesion_kawaii"]; 
 		if($contenido == null) {
