5ef575c02de3bf2af018de540f847e72ebb8ef0b
Alfonso Alvarado <Alfonso.Alvarado.v31@gmail.com>
2019-12-15 03:22:54 -0500

    Revert "Session cookies (#11)"
    This reverts commit f588cc983742ddae17a423be17b863d9319dfdf3.

--- app/app/core/controller.php
+++ app/app/core/controller.php
@@ -4,16 +4,5 @@ class controller{
 	public function view($view, $data = []){
 		require_once '../app/views/' . $view . '.phtml';
 	}
-
-	public function autenticate(){
-		$contenido = $_COOKIE["id_user_sesion_kawaii"]; 
-		if($contenido == null) {
-			//uncomment para usar login
-			//header("Location: http://localhost:8000/usuarios/login");
-			//exit;
-		}
-		
-	}
-
 }
 ?>
