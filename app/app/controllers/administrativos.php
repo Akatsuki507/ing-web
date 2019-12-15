b36f9d847e8e28601fd0c353578c610ad758e753
Akatsuki507 <Alfonso.Alvarado.v31@gmail.com>
2019-12-14 21:10:23 -0500

    edit administrativos view + method

--- app/app/controllers/administrativos.php
+++ app/app/controllers/administrativos.php
@@ -9,5 +9,15 @@ class administrativos extends controller{
 		echo 'administrativos/show';
 		$this->view('administrativos/show', []);
 	}
+	
+	public function edit($id = ''){
+		require_once("../app/models/usuario.php");
+		
+		$user=new Usuario();
+		$users=$user->where("id",$id);
+		 
+		//Llamada a la vista
+		$this->view('administrativos/edit', ['user' => $users]);
+	}
 }
 ?>
