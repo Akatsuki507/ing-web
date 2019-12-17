b309605b67f35621e7be88b490432b1e556163ce
LuisXT <blacksonic583@gmail.com>
2019-12-16 19:38:25 -0500

    Documentacion del controlador home y algunas modificaciones a ese mismo archivo

--- app/app/controllers/home.php
+++ app/app/controllers/home.php
@@ -1,11 +1,10 @@
 <?php
+//Funciona para dar la dirrecion base, en sentido de que si no vas a una ruta determinada, el programa te enviara al home de inmediato.
 class home extends controller{
 	public function index(){
-		require_once '../app/models/usuario.php';
-		$this->autenticate();
-		 
-		//Llamada a la vista
-		$this->view('home/index', []);
+		$this->autenticate(); 
+		//Llamada a la vista home, para que muestre el html del home
+		$this->view('home/index',[]);
 	}
 }
 ?>
