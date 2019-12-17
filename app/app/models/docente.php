f6ad8ea64ed7f0cd376e3a4d02c88e4a306e0dde
LuisXT <blacksonic583@gmail.com>
2019-12-16 20:25:06 -0500

    documentacion del modelo de docentes _actualizado_

--- app/app/models/docente.php
+++ app/app/models/docente.php
@@ -6,19 +6,19 @@ class Docente extends Model{
         $this->db=Conectar::conexion();
         $this->usuarios=array();
     }
-    //sirve para consultar el nombre, cedula y fecha de nacimiento
+    //insertar los datos de un docente
     public function new($nombre,$cedula, $nacimiento){
         $this->rows = null;
         $consulta=$this->db->query("insert into {$this->table}(nombre,cedula,fecha_nacimiento) values ($nombre, $cedula, $nacimiento);");
         return true;
     }
-    //sirve para consultar el id del usuario, tanto el id que usa en el programa como el que tiene registrado como docente
+    //sirve para unir el id_usuario con el id_docente del perfil recien creado
 	public function attach_user($user_id, $docente_id){
         $this->rows = null;
         $consulta=$this->db->query("update usuarios set docentes_id = $docente_id where id = $user_id;");
         return true;
     }
-    //sirve para consultar los nuevos titulos del docente que se planean agregar
+    //sirve para insertar los datos de un nuevo titulo que quiere registrar
     public function new_titulo($titulo,$year,$institucion,$grado,$docente_id){
         $this->rows = null;
         $consulta=$this->db->query("insert into preparacion_docente (nombre , year , universidad , grado , docentes_id) values ($titulo , $year, $institucion ,$grado , $docente_id);");
@@ -33,7 +33,7 @@ class Docente extends Model{
         }
         return $this->rows;
     }
-    //sirve para consultar los nuevos familiares del docente que se planean agregar
+    //sirve para ingresar los datos de un nuevo familiar registrado de un docente
     public function new_familiar($nombre,$localizar_emergencia,$prioridad_localizar,$parentesco,$fecha_nacimiento,$telefono,$correo,$docente_id){
         $this->rows = null;
         $consulta=$this->db->query("insert into famila_docentes (nombre ,localizar_emergencia , prioridad_localizar , parentesco , fecha_nacimiento , telefono , correo , docentes_id) values ($nombre ,$localizar_emergencia ,$prioridad_localizar ,$parentesco ,$fecha_nacimiento ,$telefono , $correo, $docente_id);");
